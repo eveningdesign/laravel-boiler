8684bded5d7d099249e832bb1da5adf6e7d031eb
Ryan Shuya <ryan@eveningdesign.com>
2016-08-08 15:59:02 -0600

    Generate show view

--- src/templates/views/show.blade.php
+++ src/templates/views/show.blade.php
@@ -2,7 +2,15 @@
 <?php echo "@section('content')".PHP_EOL; ?>
 <div class="row">
     <div class="col-sm-12">
-        <h3>View {!! $names->reset()->singular()->studly()->get() !!}</h3>
+        <h3>View <?php echo \EveningDesign\Boiler\Support\Helpers::makeHumanFriendly($names->reset()->singular()->get()); ?></h3>
     </div>
+    <table class="table table-bordered">
+@foreach($columns as $column)
+        <tr>
+            <td><?php echo \EveningDesign\Boiler\Support\Helpers::makeHumanFriendly($column->Field); ?></td>
+            <td><?php echo "<?php echo $".$names->getSingularInstanceName().'->'.$column->Field."; ?>"; ?></td>
+        </tr>
+@endforeach
+    </table>
 </div>
 <?php echo "@stop"; ?>
