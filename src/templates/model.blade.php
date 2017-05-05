81e1b3b7b2c3e22a6277f1cb5edb717e4bc0cfdb
Ryan Shuya <ryan@eveningdesign.com>
2017-05-05 12:00:36 -0600

    Yet another fix for Model formatting

--- src/templates/model.blade.php
+++ src/templates/model.blade.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class <?php echo $names->getModelName(); ?> extends Model
 {
-    @foreach($allColumns->pluck('Field') as $field)
+@foreach($allColumns->pluck('Field') as $field)
     <?php echo sprintf("const COL_%s = '%s';%s", strtoupper($field), $field, PHP_EOL); ?>
-    @endforeach
+@endforeach
 
     protected $fillable = [
     @foreach($filteredColumns->pluck('Field')->map(function($i) {return sprintf("self::COL_%s", strtoupper($i));}) as $column)
-        <?php echo $column; ?>,
-    @endforeach
-    ];
+    <?php echo $column; ?>,
+@endforeach
+];
 }
