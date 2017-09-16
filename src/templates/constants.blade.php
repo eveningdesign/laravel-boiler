70363d1ed1417390c9a848b6f5b6b79b9f1ef69a
Ryan Shuya <r.shuya@gmail.com>
2017-09-16 14:55:22 -0600

    Fix typo in variable name

--- src/templates/constants.blade.php
+++ src/templates/constants.blade.php
@@ -4,29 +4,29 @@ namespace App\Constants;
 class {{ $names->getConstantClass() }} {
 
     const INDEX_ROUTE = {{ sprintf("%s.index", $names->getRouteBase()) }};
-    const INDEX_ACTION = {{ sprintf("%s@index", $name->getControllerClass()) }};
+    const INDEX_ACTION = {{ sprintf("%s@index", $names->getControllerClass()) }};
     const INDEX_VIEW = {{ sprintf("%s.index", $names->getViewPath()) }};
 
     const CREATE_ROUTE = {{ sprintf("%s.create", $names->getRouteBase()) }};
-    const CREATE_ACTION = {{ sprintf("%s@create", $name->getControllerClass()) }};
+    const CREATE_ACTION = {{ sprintf("%s@create", $names->getControllerClass()) }};
     const CREATE_VIEW = {{ sprintf("%s.create", $names->getViewPath()) }};
 
     const STORE_ROUTE = {{ sprintf("%s.store", $names->getRouteBase()) }};
-    const STORE_ACTION = {{ sprintf("%s@store", $name->getControllerClass()) }};
+    const STORE_ACTION = {{ sprintf("%s@store", $names->getControllerClass()) }};
 
     const SHOW_ROUTE = {{ sprintf("%s.show", $names->getRouteBase()) }};
-    const SHOW_ACTION = {{ sprintf("%s@show", $name->getControllerClass()) }};
+    const SHOW_ACTION = {{ sprintf("%s@show", $names->getControllerClass()) }};
     const SHOW_VIEW = {{ sprintf("%s.show", $names->getViewPath()) }};
 
     const EDIT_ROUTE = {{ sprintf("%s.edit", $names->getRouteBase()) }};
-    const EDIT_ACTION = {{ sprintf("%s@edit", $name->getControllerClass()) }};
+    const EDIT_ACTION = {{ sprintf("%s@edit", $names->getControllerClass()) }};
     const EDIT_VIEW = {{ sprintf("%s.edit", $names->getViewPath()) }};
 
     const UPDATE_ROUTE = {{ sprintf("%s.update", $names->getRouteBase()) }};
-    const UPDATE_ACTION = {{ sprintf("%s@update", $name->getControllerClass()) }};
+    const UPDATE_ACTION = {{ sprintf("%s@update", $names->getControllerClass()) }};
 
     const DESTROY_ROUTE = {{ sprintf("%s.destroy", $names->getRouteBase()) }};
-    const DESTROY_ACTION = {{ sprintf("%s@destroy", $name->getControllerClass()) }};
+    const DESTROY_ACTION = {{ sprintf("%s@destroy", $names->getControllerClass()) }};
 
     const FORM_VIEW = {{ sprintf("%s._form", $names->getViewPath()) }};
 
